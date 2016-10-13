6f1113fdc1243930fda5f9c0707f6b7d98aa09eb
Ryan Frazier <frazierr2@gmail.com>
2016-10-13 15:53:25 -0400

    Added the initial settings to index.js

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,4 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var _ = require('underscore');
+var models = require(‘. / models’);
+var gameTemplate = require('../templates/gameTemplate.hbs');
