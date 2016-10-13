d6388f85ad18051e62e94ffed76b45555892e29f
Oeromilk <oero.mccomb@gmail.com>
2016-10-13 15:53:30 -0400

    added models

---
+++ app/scripts/models.js
@@ -0,0 +1,17 @@
+var $ = require('jquery');
+
+function Character(){
+
+}
+
+function Heroes(){
+
+}
+
+Heroes.prototype = new Character();
+
+function Dragons(){
+
+}
+
+Dragons.prototype = new Character();
